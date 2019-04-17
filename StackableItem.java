d5a08af4c0edc4add44d6ac0e62defbd839a99cc
Daniel Johnston <daniel.august.j@gmail.com>
2019-04-17 15:23:41 -0500

    Update StackableItem.java

--- StackableItem.java
+++ StackableItem.java
@@ -1,24 +1,31 @@
-public abstract class StackableItem implements Stackable
+public abstract class StackableItem implements Item
 {
     private final String itemName;
     private final int itemId;
     private final String itemDetails;
     private final int MAXSTACKAMOUNT;
+    private final int value;
     
-    StackableItem(String itemName, int itemId, String itemDetails, int maxamount)
+    StackableItem(String itemName, int value, int itemId, String itemDetails, int maxamount)
     {
         this.itemName = itemName;
         this.itemId = itemId;
         this.itemDetails = itemDetails;
         this.MAXSTACKAMOUNT = maxamount;
+        this.value = value;
     }
     
-    @Override
     public int maxStackAmount()
     {
         return MAXSTACKAMOUNT;
     }
-
+    
+    @Override
+    public int getValue()
+    {
+        return this.value;
+    }
+    
     @Override
     public String getDetails()
     {
@@ -36,4 +43,8 @@ public abstract class StackableItem implements Stackable
     {
         return itemId;
     }
+    
+    abstract int stackAmount();
+    abstract int addAmount();
+    abstract int removeAmount();
 }
