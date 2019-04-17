5c86d53cea57b41cb0e20da741ad934bae0bda52
Daniel Johnston <daniel.august.j@gmail.com>
2019-04-17 15:11:39 -0500

    Delete Equipable.java

--- Equipable.java
+++
@@ -1,7 +0,0 @@
-interface Equipable extends Item
-{
-    abstract String getSlotId();
-    abstract String getItemStats();
-    abstract boolean canEquit(String currentStats);
-    abstract boolean isEquip();
-}
