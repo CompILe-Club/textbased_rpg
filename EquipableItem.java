1b20f56f9da7e8ce031d70f224c1b7af0f11dfa1
Daniel Johnston <daniel.august.j@gmail.com>
2019-04-17 15:12:26 -0500

    Update EquipableItem.java

--- EquipableItem.java
+++ EquipableItem.java
@@ -1,3 +1,4 @@
+package textbasedrpg;
 //Created By: Daniel August Johnston
 //Author Title: CompILe President (Since Spring 2019)
 //Date of Completion: 14 February 2019
@@ -5,7 +6,7 @@
 //Company or Organization: CompILe @ Illinois Central College East Peoria, IL
 //Purpose: This class defines what an equipable item is within the overall class of items.
 
-public class EquipableItem implements Equipable
+public class EquipableItem implements Item
 {
     //Holds the item name and can not be changed after the instance of a EquipableItem has been made.
     private final String itemName;
@@ -28,11 +29,21 @@ public class EquipableItem implements Equipable
     private final String itemDetails;
     //Tells if the item is currently equipped or not and can not be changed after the instance of a EquipableItem has been made.
     private boolean equipped;
+    //Holds the damage range of weapons
+    private final int damage;
+    //Holds the armor value of armor
+    private final int armor;
+    //Tells if the equippable item is armor or weapon
+    private final boolean isWeapon;
+    //Zero array
+    private final int[] ZERO = {0,0};
+    private final int value;
     
     //Initializing the item with the desired information input when calling this
     //constructor.
-    EquipableItem(String itemName, int itemID, String slotId, String requirements, 
-            String itemStats, String itemDetails, boolean equipped)
+    EquipableItem(String itemName, int value,int itemID, String slotId, String requirements, 
+            String itemStats, String itemDetails, boolean equipped, int damage
+            ,boolean isWeapon, int armor)
     {
         this.itemName = itemName;
         this.itemId = itemID;
@@ -41,17 +52,49 @@ public class EquipableItem implements Equipable
         this.itemStats = itemStats;
         this.itemDetails = itemDetails;
         this.equipped = equipped;
+        this.damage = damage;
+        this.isWeapon = isWeapon;
+        this.armor = armor;
+        this.value = value;
     }
     
     //Returns a boolean value true if the item is currently equipped and false if not.
-    @Override
     public boolean isEquip()
     {
         return equipped;
     }
     
-    //Returns the string of stat bonuses given by this item.
+    //Returns if this is a weapon or armor
+    public boolean isWeapon()
+    {
+        return isWeapon;
+    }
+    
+    //Returns the armor if its armor
+    public int getArmor()
+    {
+        if(isWeapon)
+            return 0;
+        else
+            return armor;
+    }
+    
+    //Returns the damage range if its a weapon
+    public int getDamage(int str)
+    {
+        if(isWeapon)
+            return damage * (str/6);
+        else
+            return 0;
+    }
+    
     @Override
+    public int getValue()
+    {
+        return this.value;
+    }
+    
+    //Returns the string of stat bonuses given by this item.
     public String getItemStats()
     {
         return itemStats;
@@ -79,24 +122,47 @@ public class EquipableItem implements Equipable
     }
     
     //Returns the slot type of this item.
-    @Override
     public String getSlotId()
     {
         return slotId;
     }
     
     //Checks to see if a player can wear this item or not.
-    @Override
-    public boolean canEquit(String currentStats)
+    public boolean canEquip(String currentStats)
     {
-        return currentStats.equalsIgnoreCase(requirements);
+        String []itemStats = requirements.split(";");
+        String []playerStats = currentStats.split(";");
+        boolean isGood = true;
+        for(int indexStats = 0; indexStats < playerStats.length; indexStats++)   
+            if(Integer.parseInt(itemStats[indexStats]) > Integer.parseInt(playerStats[indexStats]))
+                isGood = false;
+        return isGood;
+    }
+    
+    public boolean equip()
+    {
+        if(this.equipped)
+            return this.equipped = false;
+        else
+            return this.equipped = true;
     }
     
     //Returns the items complete detailed information
     @Override
     public String toString()
     {
-        return String.format("%s\n\tItem Stats:\n\t%s\nItem Requirements:\n\t%s\nItem Details:\n\t%s\n"
-                , itemName, itemStats,requirements, itemDetails);
+        return String.format("%s\n\tItem Stats:\n\t%s\n\tItem Slot:\n\t%s\n\tItem Requirements:\n\t%s\n\tItem Details:\n\t%s\n"
+                , this.itemName, this.itemStats, this.slotId, this.requirements, this.itemDetails);
+    }
+    
+    public String returnToTextFile()
+    {
+        int weapon = 0;
+        if(this.isWeapon)
+            weapon = 1;
+        return String.format("E#%s#%d#%s#%s#%s#%s#%d#%d#%d#%d", 
+                this.itemName, this.value, this.slotId, this.requirements
+                , this.itemStats, this.itemDetails, 0, this.damage, 
+                weapon, this.armor);
     }
 }
