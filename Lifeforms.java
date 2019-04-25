b969d9081baac89a74920245717a1a167a97e86b
Liam Andrade <LegoLink25@gmail.com>
2019-04-25 10:25:11 -0500

    Update and rename LIFEFORMS_INTERFACE to Lifeforms.java
    Changed filename and some functions so the game would compile.

--- Lifeforms.java
+++ Lifeforms.java
@@ -3,10 +3,10 @@ public interface Lifeforms {
     abstract int [] getStats();
     abstract String getName();
     abstract int getLocation();
-    abstract Item[] getInventory();
+    abstract Inventory getInventory();
     abstract int getHealth();
     abstract int getMana();
-    abstract int attack(Lifeforms target);
+    abstract int attack(Lifeforms target, World currentWorld);
     abstract int accuracy(Lifeforms target);
     abstract int getLevel();
     
