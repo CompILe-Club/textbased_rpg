c0d1638c76dc93131b8f535e75f87ef41e3825ab
Daniel Johnston <daniel.august.j@gmail.com>
2019-04-25 17:36:08 -0500

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -23,6 +23,7 @@ public class Grid
     private final String details;
     private final int gridID;
     private final String locationName;
+    private Item[] droppedItems;
     
     Grid(GameObjects [] objects, Creature [] creatures, NPC [] people, Structure [] buildings,
             String details, int gridID, String locationName)
@@ -50,8 +51,55 @@ public class Grid
         this.details= details;
         this.gridID = gridID;
         this.locationName = locationName;
+        this.droppedItems = new Item[0];
     }
 
+    public Item[] getDrops()
+    {
+        return this.droppedItems;
+    }
+    
+    public void addItem(Item droppedItem)
+    {
+        Item tempItems[] = new Item[this.droppedItems.length + 1];
+        for(int indexItems = 0; indexItems < this.droppedItems.length; indexItems++)
+            tempItems[indexItems] = this.droppedItems[indexItems];
+        tempItems[this.droppedItems.length] = droppedItem;
+        this.droppedItems = tempItems;
+    }
+    
+    public void removeItem(String itemName)
+    {
+        if(this.droppedItems.length != 0)
+        {
+            Item tempItems[] = new Item[this.droppedItems.length - 1];
+            for(int indexItems = 0; indexItems < this.droppedItems.length; indexItems++)
+            {
+                if(droppedItems[indexItems].getItemName().equalsIgnoreCase(itemName))
+                {
+                     for(int indexItems2 = indexItems + 1; indexItems2 < this.droppedItems.length; indexItems2++)
+                     {
+                         tempItems[indexItems2 - 1] = this.droppedItems[indexItems2];
+                     }
+                     this.droppedItems = tempItems;
+                     break;
+                }
+                else if(indexItems != this.droppedItems.length - 1)
+                    tempItems[indexItems] = this.droppedItems[indexItems];
+            }
+        }
+    }
+    
+    public Item pickUpItem(String itemName)
+    {
+            for(int indexItems = 0; indexItems < this.droppedItems.length; indexItems++)
+            {
+                if(droppedItems[indexItems].getItemName().equalsIgnoreCase(itemName))
+                     return this.droppedItems[indexItems];
+            }
+        return null;
+    }
+    
     public Grid getNorth() {
         return north;
     }
@@ -211,35 +259,36 @@ public class Grid
     public String searchArea()
     {
         String outputString = "";
-        if((this.creatures[0] != null))
+        if((this.creatures[0] != null) && !(this.creatures[0].isIsdead()))
             outputString += String.format("Creature(s):\n");
         for(int indexCreatures = 0; indexCreatures < this.creatures.length; indexCreatures++)
         {
-            if(!(this.creatures[indexCreatures].isIsdead()))
-                outputString += String.format("[%d] %s", indexCreatures, this.creatures[indexCreatures].getName());
+            if((this.creatures[indexCreatures] != null) && !(this.creatures[indexCreatures].isIsdead()))
+                outputString += String.format("\t%s\n", this.creatures[indexCreatures].getName());
         }
         if((this.buildings[0] != null))
             outputString += String.format("\nStructure(s):\n");
         for(int indexBuildings = 0; indexBuildings < this.buildings.length; indexBuildings++)
         {
             if((this.buildings[indexBuildings] != null))
-                outputString += String.format("[%d] %s", indexBuildings, this.buildings[indexBuildings]);
+                outputString += String.format("\t%s\n", this.buildings[indexBuildings]);
         }
         if((this.objects[0] != null))
          outputString += String.format("\nItem(s):\n");
         for(int indexObjects = 0; indexObjects < this.objects.length; indexObjects++)
         {
             if((this.objects[indexObjects] != null))
-                outputString += String.format("[%d] %s", indexObjects, this.objects[indexObjects]);
+                outputString += String.format("\t%s\n", this.objects[indexObjects]);
         }
         if((this.NPCs[0] != null))
             outputString += String.format("\nPeople:\n");
         for(int indexNPCs = 0; indexNPCs < this.NPCs.length; indexNPCs++)
         {
             if((this.NPCs[indexNPCs] != null))
-                outputString += String.format("[%d] %s", indexNPCs, this.NPCs[indexNPCs]);
+                outputString += String.format("\t%s\n", this.NPCs[indexNPCs].getName());
         }
-        outputString += String.format("\n");
+        if(outputString.equalsIgnoreCase(""))
+            outputString = String.format("There is currently nothing here.\n");
         return outputString;
     }
     
